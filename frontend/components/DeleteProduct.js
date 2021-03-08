159e6cde66e4b5bc16e32706b393e4f88d46ff42
Naveen <nav33nk>
2021-03-08 20:19:57 +0530

    remove deleted item from cache

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -5,15 +5,21 @@ const DELETE_PRODUCT_MUTATION = gql`
   mutation DELETE_PRODUCT_MUTATION($id: ID!) {
     deleteProduct(id: $id) {
       id
+      name
     }
   }
 `;
+
+const update = (cache, payload) => {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+};
 
 export default function DeleteProduct({ id, children }) {
   const [deleteProduct, { loading, error }] = useMutation(
     DELETE_PRODUCT_MUTATION,
     {
       variables: { id },
+      update: update,
     }
   );
 
