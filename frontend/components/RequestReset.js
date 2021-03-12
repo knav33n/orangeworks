e1cac2425d57467808ba8bdb44ed912aaa274338
Naveen <nav33nk>
2021-03-12 06:44:25 +0530

    add sign up
    forgot password - wip

---
+++ frontend/components/RequestReset.js
@@ -0,0 +1,56 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import useForm from "../lib/useForm";
+import DisplayError from "./ErrorMessage";
+import Form from "./styles/Form";
+
+const REQUEST_RESET_MUTATION = gql`
+  mutation REQUEST_RESET_MUTATION($email: String!) {
+    sendUserPasswordResetLink(email: $email) {
+      code
+      message
+    }
+  }
+`;
+
+export default function RequestReset() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: "",
+  });
+  const [signup, { data, loading, error }] = useMutation(
+    REQUEST_RESET_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await signup().catch(console.error);
+    resetForm();
+  };
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Request Password Reset</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        {data?.sendUserPasswordResetLink === null && (
+          <p>Success! Check your email for a link!</p>
+        )}
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Request Reset!</button>
+      </fieldset>
+    </Form>
+  );
+}
---
+++ frontend/components/SignUp.js
@@ -0,0 +1,80 @@
+import { useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import useForm from "../lib/useForm";
+import DisplayError from "./ErrorMessage";
+import Form from "./styles/Form";
+
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+export default function SignUp() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: "",
+    name: "",
+    password: "",
+  });
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
+    variables: inputs,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await signup().catch(console.error);
+    resetForm();
+  };
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Up for an Account</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        {data?.createUser && <p>Signed up with {data.createUser.email}</p>}
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign Up!</button>
+      </fieldset>
+    </Form>
+  );
+}
