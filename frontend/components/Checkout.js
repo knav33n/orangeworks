0254cdc1d930a1933adeda369458533d439c0baa
Naveen <nav33nk>
2021-03-22 21:18:16 +0530

    checkout - wip

---
+++ frontend/components/Checkout.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import SickButton from "./styles/SickButton";
+import { useState } from "react";
+import nProgress, { done } from "nprogress";
+
+const CheckoutFormStyles = styled.form`
+  box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
+  border: 1px solid rgba(0, 0, 0, 0.06);
+  border-radius: 5px;
+  padding: 1rem;
+  display: grid;
+  grid-gap: 1rem;
+`;
+
+// https://stripe.com/docs/testing
+const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+
+const CheckoutForm = () => {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    nProgress.start();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+    console.log(paymentMethod);
+    if (error) {
+      setError(error);
+    }
+    // TODO: Sthuff
+    setLoading(false);
+    nProgress.done();
+  };
+
+  return (
+    <CheckoutFormStyles onSubmit={handleSubmit}>
+      {error && <p style={{ fontSize: 12 }}>{error.message}</p>}
+      <CardElement />
+      <SickButton>Checkout Now</SickButton>
+    </CheckoutFormStyles>
+  );
+};
+
+const Checkout = () => {
+  return (
+    <Elements stripe={stripeLib}>
+      <CheckoutForm />
+    </Elements>
+  );
+};
+
+export default Checkout;
